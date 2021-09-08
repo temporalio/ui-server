ca4ef50a4aae0b861f5a579a6dcfc42db10b424a
feedmeapples <aksenov.ro@outlook.com>
2021-09-07 17:18:11 -0700

    Clean out react based PoC UI

--- webui/src/features/routes/index.ts
+++
@@ -1,5 +0,0 @@
-export { Namespaces } from "./namespaces";
-export { Workflows } from "./workflows";
-export { NamespacesList } from "./namespaces-list";
-export { WorkflowsList } from "./workflows-list";
-export { Account } from "./account";
