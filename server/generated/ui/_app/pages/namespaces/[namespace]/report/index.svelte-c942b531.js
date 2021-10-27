51c7202f406cd3ad84c565161de7a141a859d904
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2021-10-27 10:02:51 -0700

    Include UI static files (#66)

---
+++ server/generated/ui/_app/pages/namespaces/[namespace]/report/index.svelte-c942b531.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as n}from"../../../../chunks/vendor-b03dee8a.js";class l extends s{constructor(e){super();t(this,e,null,null,n,{})}}export{l as default};
---
+++ server/generated/ui/_app/pages/namespaces/index.svelte-c48b700c.js
@@ -0,0 +1,1 @@
+import{S as s,i as a,s as t}from"../../chunks/vendor-b03dee8a.js";async function r(){return{status:302,redirect:"/namespaces/default"}}class u extends s{constructor(e){super();a(this,e,null,null,t,{})}}export{u as default,r as load};
