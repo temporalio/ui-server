1ecf77f6992072b97a2872a5fa6253cebc1546c7
feedmeapples <aksenov.ro@outlook.com>
2022-03-02 16:56:34 -0500

    Update UI paths for golang v1.17

--- server/generated/ui/_app/chunks/get-context-637b8da5.js
+++
@@ -1,1 +0,0 @@
-import{af as e}from"./vendor-07bbbbe5.js";function n(t){return e(t)}export{n as g};
--- server/generated/ui/_app/chunks/navigation-51f4a605.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./singletons-12a22614.js";const n=r,g=s;async function s(o,t){return n.goto(o,t,[])}export{g};
