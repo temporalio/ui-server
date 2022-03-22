6a32f1a7a895027c8579cb4c81dd7daf23c9000f
Temporal Data (cicd) <commander-data@temporal.io>
2022-03-22 23:04:10 +0000

    Update UI submodule

--- server/generated/ui/_app/chunks/get-context-148b86cd.js
+++
@@ -1,1 +0,0 @@
-import{ad as e}from"./vendor-9a5c5afd.js";function n(t){return e(t)}export{n as g};
---
+++ server/generated/ui/_app/chunks/get-context-586e8b7b.js
@@ -0,0 +1,1 @@
+import{ad as e}from"./vendor-79e2f0e4.js";function n(t){return e(t)}export{n as g};
