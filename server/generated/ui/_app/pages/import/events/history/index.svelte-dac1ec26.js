603664bb78758ac6525a18b9d723273793373154
Temporal Data (cicd) <commander-data@temporal.io>
2022-04-12 16:13:11 +0000

    Update UI submodule

---
+++ server/generated/ui/_app/pages/import/events/history/index.svelte-dac1ec26.js
@@ -0,0 +1,1 @@
+import{S as o,i as e,s as r}from"../../../../chunks/index-4f955b1f.js";import{c as n}from"../../../../chunks/route-for-a8f6a277.js";const u=async function(){const t=n({importType:"events",view:"summary"});return{status:302,redirect:t}};class c extends o{constructor(s){super(),e(this,s,null,null,r,{})}}export{c as default,u as load};
