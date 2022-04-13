ce2e1e3fbd49ab2ed058854a82dbf05eb80b9b9b
Temporal Data (cicd) <commander-data@temporal.io>
2022-04-13 02:45:18 +0000

    Update UI submodule

---
+++ server/generated/ui/_app/pages/import/index.svelte-1c35d3ac.js
@@ -0,0 +1,1 @@
+import{S as o,i as e,s as r}from"../../chunks/index-4f955b1f.js";import{c as n}from"../../chunks/route-for-d606e623.js";const p=async()=>{const t=n({importType:"events"});return{status:302,redirect:t}};class u extends o{constructor(s){super(),e(this,s,null,null,r,{})}}export{u as default,p as load};
