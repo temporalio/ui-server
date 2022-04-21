9b274ac6d175c6e29322a9d0034c0e9af017e7ca
Temporal Data (cicd) <commander-data@temporal.io>
2022-04-21 14:23:05 +0000

    Update UI submodule

--- server/generated/ui/_app/chunks/event-view-type-4ec3ffa7.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./persist-store-789d3dc3.js";import{k as t}from"./route-for-8819f00e.js";const i=s("eventView","summary"),p=e=>{t(e)&&i.set(e)};export{i as e,p as s};
---
+++ server/generated/ui/_app/chunks/event-view-type-f41db6ca.js
@@ -0,0 +1,1 @@
+import{p as s}from"./persist-store-789d3dc3.js";import{j as t}from"./route-for-46a9c4b7.js";const i=s("eventView","summary"),p=e=>{t(e)&&i.set(e)};export{i as e,p as s};
