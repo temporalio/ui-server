13e7172285201300726fbe2ccf930c75f051a15d
Temporal Data (cicd) <commander-data@temporal.io>
2022-05-25 21:11:11 +0000

    Update UI submodule

--- server/generated/ui/_app/chunks/import-events-85ab41f6.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./index-8e6fc7ce.js";const r=t([]),s=t([]);export{s as a,r as i};
--- server/generated/ui/_app/pages/import/events/history/index.svelte-0028e7bf.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as o,s as r}from"../../../../chunks/index-6f949d33.js";import{d as n}from"../../../../chunks/route-for-9a2d1abe.js";const u=async function(){const t=n({importType:"events",view:"feed"});return{status:302,redirect:t}};class l extends s{constructor(e){super(),o(this,e,null,null,r,{})}}export{l as default,u as load};
