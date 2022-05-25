146bdbc9ba12799e00fd5c9b25f4e3f542d973a1
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2022-05-25 10:36:21 -0400

    Bump version to v0.15.0 (#149)

--- server/version/version.go
+++ server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "0.14.0"
+	UIVersion               = "0.15.0"
 )
