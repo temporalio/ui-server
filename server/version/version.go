fa8d2508dceb5194bbf0fa28a6a6811b10dacf35
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2022-09-15 13:53:08 -0400

    Bump version to v2.6.1 (#226)

--- server/version/version.go
+++ server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.6.0"
+	UIVersion               = "2.6.1"
 )
