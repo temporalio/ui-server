7dd2932a4c3f04b7873a5a1e9c029fef6e3d0467
Temporal Data (cicd) <commander-data@temporal.io>
2022-09-20 19:58:38 +0000

    Update UI submodule

---
+++ server/generated/ui/_app/immutable/chunks/new-feature-tags-f278f9ba.js
@@ -0,0 +1,1 @@
+import{a6 as r}from"./index-8e660855.js";import{p as o}from"./persist-store-09ca1357.js";const a=o("viewedFeatureTags",null),l=s=>{var t;let e=(t=r(a))!=null?t:[];e.includes(s)||(e=[...e,s],a.set(e))};export{l as a,a as v};
