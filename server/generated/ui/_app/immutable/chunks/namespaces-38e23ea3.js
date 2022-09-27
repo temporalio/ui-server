528b17efdd47668b4bb2c20a2ad283b1d35a2907
Temporal Data (cicd) <commander-data@temporal.io>
2022-09-27 21:33:51 +0000

    Update UI submodule

--- server/generated/ui/_app/immutable/chunks/namespaces-38e23ea3.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./persist-store-09ca1357.js";const e=s("lastNamespace","default");export{e as l};
--- server/generated/ui/_app/immutable/chunks/time-format-bdd9716f.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./persist-store-09ca1357.js";const o=s("timeFormat","UTC"),m=t=>{o.set(t)};export{m as s,o as t};
