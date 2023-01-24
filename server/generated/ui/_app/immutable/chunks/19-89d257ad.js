212a337ff152d151934bba6358da07c7a83296f7
Temporal Data (cicd) <commander-data@temporal.io>
2023-01-24 22:47:29 +0000

    Update UI submodule

---
+++ server/generated/ui/_app/immutable/chunks/19-89d257ad.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(app)/namespaces/_namespace_/task-queues/_queue_/_page.svelte-e2e12e0f.js";export{t as component};
--- server/generated/ui/_app/immutable/chunks/19-a22aee14.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/(app)/namespaces/_namespace_/task-queues/_queue_/_page.svelte-93961e50.js";export{t as component};
