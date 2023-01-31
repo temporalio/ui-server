ee3e7613363522872ee9804d3fac39b5e956de76
Ross Edfort <rossedfort@gmail.com>
2023-01-31 13:03:48 -0700

    add config option for disabling bulk actions

--- server/api/handler.go
+++ server/api/handler.go
@@ -62,6 +62,7 @@ type SettingsResponse struct {
 	WorkflowCancelDisabled      bool
 	WorkflowSignalDisabled      bool
 	WorkflowResetDisabled       bool
+	BulkActionsDisabled         bool
 }
 
 func TemporalAPIHandler(cfgProvider *config.ConfigProviderWithRefresh, apiMiddleware []Middleware) echo.HandlerFunc {
@@ -127,6 +128,7 @@ func GetSettings(cfgProvier *config.ConfigProviderWithRefresh) func(echo.Context
 			WorkflowCancelDisabled:    cfg.WorkflowCancelDisabled,
 			WorkflowSignalDisabled:    cfg.WorkflowSignalDisabled,
 			WorkflowResetDisabled:     cfg.WorkflowResetDisabled,
+			BulkActionsDisabled:       cfg.BulkActionsDisabled,
 		}
 
 		return c.JSON(http.StatusOK, settings)
