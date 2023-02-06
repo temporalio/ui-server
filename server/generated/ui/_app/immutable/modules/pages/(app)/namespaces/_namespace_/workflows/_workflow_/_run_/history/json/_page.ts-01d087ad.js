e5c3943b59a9640a8ba2347c44dd11d90e9d67a9
Temporal Data (cicd) <commander-data@temporal.io>
2023-02-06 16:42:37 +0000

    Update UI submodule

--- server/generated/ui/_app/immutable/modules/pages/(app)/namespaces/_namespace_/workflows/_workflow_/_run_/history/json/_page.ts-01d087ad.js
+++ server/generated/ui/_app/immutable/modules/pages/(app)/namespaces/_namespace_/workflows/_workflow_/_run_/history/json/_page.ts-01d087ad.js
@@ -1,1 +1,1 @@
-import"../../../../../../../../../../chunks/index-dc735eee.js";import"../../../../../../../../../../chunks/route-for-9e2a964e.js";import{l as t}from"../../../../../../../../../../chunks/_page-367cd8be.js";export{t as load};
+import"../../../../../../../../../../chunks/index-dc735eee.js";import"../../../../../../../../../../chunks/route-for-9e2a964e.js";import{l as t}from"../../../../../../../../../../chunks/_page-eba074cd.js";export{t as load};
