2c5962ecec623c77d576557d2a60cacc2bf8c1d3
Temporal Data (cicd) <commander-data@temporal.io>
2023-03-01 23:04:38 +0000

    Update UI Server

---
+++ ui/assets/_app/immutable/chunks/2-32564480.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-5a229169.js";import{default as t}from"../components/pages/(app)/_layout.svelte-1b6a7941.js";export{t as component,r as universal};
---
+++ ui/assets/_app/immutable/chunks/7-1dca0860.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(app)/namespaces/_namespace_/workflows/_workflow_/_run_/_layout.svelte-0c958072.js";export{t as component};
