1af21dcc7b1cc87882f833964dc1732cafd9348b
Temporal Data (cicd) <commander-data@temporal.io>
2024-01-16 17:13:58 +0000

    Update UI Server

---
+++ ui/assets/local/_app/immutable/chunks/import-events.15c59d3c.js
@@ -0,0 +1,1 @@
+import{w as t}from"./singletons.03d5a6ca.js";const r=t([]),s=t([]);export{s as a,r as i};
---
+++ ui/assets/local/_app/immutable/chunks/navigation.10e99bb1.js
@@ -0,0 +1,1 @@
+import{h as t}from"./singletons.03d5a6ca.js";const o=t("goto"),e=t("after_navigate");export{e as a,o as g};
