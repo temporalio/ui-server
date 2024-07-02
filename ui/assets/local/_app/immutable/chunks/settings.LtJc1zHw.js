b2e3b4d5e76e7d17a93e57881b956911f0cf27f9
Brendan Gerrity <brendan.gerrity@datadoghq.com>
2024-07-02 13:40:30 -0400

    Update UI Server

---
+++ ui/assets/local/_app/immutable/chunks/settings.LtJc1zHw.js
@@ -0,0 +1,1 @@
+import{d as t}from"./entry.IB1Wy_Z1.js";import{p as a}from"./stores.0S-p236-.js";const r=t([a],([s])=>s.data.settings);export{r as s};
---
+++ ui/assets/local/_app/immutable/entry/start.RabVsIEZ.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/entry.IB1Wy_Z1.js";export{a as start};
