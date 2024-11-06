8521ea78ff40d6c9b009c2a867fb157ea44b70c1
Temporal Data (cicd) <commander-data@temporal.io>
2024-11-06 17:18:29 +0000

    Update UI Server

--- ui/assets/local/_app/immutable/chunks/namespaces.6QyxPm7y.js
+++
@@ -1,1 +0,0 @@
-import{w as s}from"./entry.qE2v-Ohg.js";import{p as a}from"./persist-store.m522rLCG.js";const r=a("lastNamespace","default",!0),o=s([]);export{r as l,o as n};
---
+++ ui/assets/local/_app/immutable/entry/start.B5Qz3Dt4.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/entry.zuIxrJek.js";export{a as start};
