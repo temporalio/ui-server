b7fdc8d01837197be4ef1212c67ca8ce4ef4c958
Temporal Data (cicd) <commander-data@temporal.io>
2025-04-17 17:18:45 +0000

    Update UI Server

---
+++ ui/assets/local/_app/immutable/chunks/S6Q2Q0ub.js
@@ -0,0 +1,1 @@
+const E={BOOL:"Bool",DATETIME:"Datetime",DOUBLE:"Double",INT:"Int",KEYWORD:"Keyword",TEXT:"Text",KEYWORDLIST:"KeywordList",UNSPECIFIED:"Unspecified"};export{E as S};
--- ui/assets/local/_app/immutable/entry/start.BwA1BXpr.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/B8CPpa6h.js";export{o as load_css,r as start};
