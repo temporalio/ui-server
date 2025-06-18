be84e32b2c0af5209d76700a620ae57bc2669751
Temporal Data (cicd) <commander-data@temporal.io>
2025-06-18 14:42:37 +0000

    Sync from UI release v2.38.3

---
+++ ui/assets/local/_app/immutable/chunks/CadMxnDr.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1r3q9tc)==null?void 0:s.base)??"";var t;const e=((t=globalThis.__sveltekit_1r3q9tc)==null?void 0:t.assets)??a;export{e as a,a as b};
---
+++ ui/assets/local/_app/immutable/entry/start.Dkxbfxs9.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/Cqkd3idL.js";export{o as load_css,r as start};
